e7e3d634be641b7576ad81d62636676096adde8c
Karina Brian <bkarinamuraya@gmail.com>
2025-05-12 18:26:00 +0300

    Add login index page tests

--- src/test/java/objects/Login.java
+++ src/test/java/objects/Login.java
@@ -1,7 +1,6 @@
 package objects;
 
 import com.mysql.cj.exceptions.AssertionFailedException;
-import general.CommonAssertions;
 import general.MainCall;
 import org.openqa.selenium.By;
 
@@ -14,6 +13,15 @@ public class Login {
     public static By login_password = By.xpath("//input[@name='password']");
     public static By loginVerification = By.xpath("//div[@class='welcome-container']");
     public static By errorMsg = By.xpath("//p[@class='error-message']");
+    public static By firstNameField = By.xpath("//input[@id='firstName']");
+    public static By favTeam = By.xpath("//option[@value='Napoli']");
+    public static By submitButton = By.xpath("//button[text()='Submit']");
+    public static By badgeConfirmation = By.xpath("//span[@id='teamMessage']");
+    public static By logoutButton = By.xpath("//form[@action='/logout']/button");
+    public static By loginPage = By.xpath("//div[@class='login-container']");
+
+
+
 
 
     public static void arriveToLogin()
@@ -23,6 +31,26 @@ public class Login {
         } catch (Exception exception) {
             throw new AssertionFailedException(exception.getMessage());
         }
+    }
+    public static void verifyUserIsLoggedOut () {
+        MainCall.seleniumFunctions.isExist(loginPage);
+    }
+    public static void clickOnLogoutBtn () {
+        MainCall.seleniumFunctions.ClickingOn(logoutButton);
+    }
+    public static void verifyFanMessage() {
+        MainCall.seleniumFunctions.isExist(badgeConfirmation);
+    }
+    public static void clickOnSubmit() {
+        MainCall.seleniumFunctions.ClickingOn(submitButton);
+    }
+    public static void selectFavouriteTeam () throws InterruptedException {
+        MainCall.seleniumFunctions.scrollToElement(favTeam);
+        Thread.sleep(3000);
+        MainCall.seleniumFunctions.ClickingOn(favTeam);
+    }
+    public  static void enterFirstName() {
+        MainCall.seleniumFunctions.IhaveGivenInput(firstNameField, "Brian");
     }
     public static void enterUserName(String userName)
     {
